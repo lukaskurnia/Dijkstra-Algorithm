6064c6a4fed9db71d78276e7d71bca563e5439c5
lukaskurnia <lukasjonathan99@gmail.com>
2019-04-25 20:32:51 +0700

    Add contoh dan hasil uji

--- bandung.py
+++ bandung.py
@@ -7,7 +7,8 @@ class Kota():
     #Constructor kota
     def __init__(self, jumlahPersimpangan):
         self.node =jumlahPersimpangan
-        self.graph = [[0 for column in range(jumlahPersimpangan)] for row in range(5)]
+        # self.graph = [[0 for column in range(jumlahPersimpangan)] for row in range(5)]
+        self.graph =[]
         self.namaPersimpangan = {}
 
     def getPersimpangan(self,idx):
@@ -29,17 +30,16 @@ class Kota():
     def printAllPath(self,start,listDistance,listRute):
         print('Jarak optimum dari {} ke seluruh tempat lain: ' .format(self.getPersimpangan(start)))
         for i in range(len(listDistance)):
-            print(self.getPersimpangan(i) , '=' , listDistance[i] , 'dengan rute : ' , end ='')
+            print((i+1),self.getPersimpangan(i) , '=' , listDistance[i] , 'dengan rute : ' )
             for indeks in range (len(listRute[i])):
-                print(self.getPersimpangan(listRute[i][indeks]), end=' ')
-            print()
+                print('-', self.getPersimpangan(listRute[i][indeks]))
+           
           
     def printPath(self,start, end, listDistance,listRute):
-        print('Jarak optimum dari {} ke {} = {} ' .format(self.getPersimpangan(start),self.getPersimpangan(end), listDistance[start]))
-        print('Dengan rute: ',end ='')
+        print('Jarak optimum dari {} ke {} = {} ' .format(self.getPersimpangan(start),self.getPersimpangan(end), listDistance[end]))
+        print('Dengan rute: ')
         for indeks in range (len(listRute[end])):
-            print(self.getPersimpangan(listRute[end][indeks]), end=' ')
-        print()
+            print('-', self.getPersimpangan(listRute[end][indeks]))
 
     def Dijkstra(self,start):
         listDistance = [99999] * self.node
@@ -62,35 +62,42 @@ class Kota():
 
         return listDistance,listRute
 
+bandung = Kota(16)
+bandung.namaPersimpangan[0]='Rumah Penulis'
+bandung.namaPersimpangan[1]='Alun-alun kota Bandung'
+bandung.namaPersimpangan[2]='Perempatan Cibaduyut – Leuwi Panjang'
+bandung.namaPersimpangan[3]='Perempatan Moh. Toha – Soekarno-Hatta'
+bandung.namaPersimpangan[4]='Pertigaan Leuwi Panjang - Peta'
+bandung.namaPersimpangan[5]='Perempatan Kopo – Soekarno-Hatta'
+bandung.namaPersimpangan[6]='Perempatan Peta – Kopo'
+bandung.namaPersimpangan[7]='Pertigaan Pasir Koja – Kopo'
+bandung.namaPersimpangan[8]='Pertigaan Pasir Koja – Astana Anyar'
+bandung.namaPersimpangan[9]='Perempatan Pungkur – Otista'
+bandung.namaPersimpangan[10]='Perempatan Peta – Inhoftank'
+bandung.namaPersimpangan[11]='Perempatan BKR – Moh. Toha'
+bandung.namaPersimpangan[12]='Pertigaan Moh. Toha – Pungkur'
+bandung.namaPersimpangan[13]='Pertigaan Dewi Sartika – Pungkur'
+bandung.namaPersimpangan[14]='Perempatan Asia Afrika – Otista'
+bandung.namaPersimpangan[15]='Perempatan Sudirman – Astana Anyar'
 
 
-bandung = Kota(7)
-
-bandung.namaPersimpangan[0]='Bandung'
-bandung.namaPersimpangan[1]='Jakarta'
-bandung.namaPersimpangan[2]='Makassar'
-bandung.namaPersimpangan[3]='Semarang'
-bandung.namaPersimpangan[4]='Surabaya'
-bandung.namaPersimpangan[5]='Malang'
-bandung.namaPersimpangan[6]='Cianjur'
-
-# print(bandung.namaPersimpangan[5])
-
-# bandung.graph = [[0, 4, 2, 0, 0, 0], 
-#                 [4, 0, 1, 5, 0, 0], 
-#                 [2, 1, 0, 8, 10, 0], 
-#                 [0, 5, 8, 0, 2, 6], 
-#                 [0, 0, 10, 2, 0, 3],
-#                 [0, 0, 0, 6, 3, 0]
-#                 ]
-            
-bandung.graph = [[0, 0, 5, 6, 0, 0, 0], 
-                [0, 0, 0, 0, 1, 3, 0], 
-                [5, 0, 0, 2, 9, 0, 3], 
-                [6, 0, 2, 0, 0, 7, 7], 
-                [0, 1, 9, 0, 0, 2, 9],
-                [0, 3, 0, 7, 2, 0, 8],
-                [0, 0, 3, 7, 9, 8, 0]
+bandung.graph = [
+                [0, 0, 1700, 1800, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 500, 0],
+                [1700, 0, 0, 1700, 1000, 650, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1800, 0, 1700, 0, 0, 0, 0, 0, 0, 0, 0, 1200, 0, 0, 0, 0], 
+                [0, 0, 1000, 0, 0, 0, 200, 0, 0, 0, 700, 0, 0, 0, 0, 0],
+                [0, 0, 650, 0, 0, 0, 1200, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 200, 0, 0, 1200, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 180, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 450, 0, 0, 0, 0, 0, 750],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1100, 0, 350, 250, 0, 0],
+                [0, 0, 0, 0, 700, 0, 0, 0, 0, 0, 0, 300, 0, 0, 0, 0],
+                [0, 0, 0, 1200, 0, 0, 0, 0, 0, 0, 300, 0, 1100, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 350, 0, 0, 0, 120, 0, 0],
+                [0, 750, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 700, 0, 0, 0, 0, 0, 650],
+                [0, 0, 0, 0, 0, 0, 0, 0, 750, 0, 0, 0, 0, 0, 0, 0]
                 ]
 
 distance,rute = bandung.Dijkstra(0)
